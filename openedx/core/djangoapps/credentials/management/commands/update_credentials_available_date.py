44140e9b251c0646a3956349cd9c7ca0ce21a4a2
Justin Hynes <Jhynes@edx.org>
2024-06-07 15:01:37 +0000

    temp-stash

--- openedx/core/djangoapps/credentials/management/commands/update_credentials_available_date.py
+++ openedx/core/djangoapps/credentials/management/commands/update_credentials_available_date.py
@@ -2,9 +2,9 @@
 A manangement command to populate the `certificate_available_date` field of the CourseCertificateConfiguration model in
 the Credentials IDA.
 
-This command is designed to be ran once to initially backpopulate data. Otherwise, anytime an existing course run
-adjusts its certificate available date or certificates display behavior settings, updates will automatically be queued
-and transmit to the Credentials IDA.
+This management command is required when moving from using `visible_date` to the certificate available date (a field of
+the CourseCertificateConfiguration model) for managing certificate visibility. It can also be run ad-hoc to fix up any
+data inconsistencies that may occur in the Credentials data.
 """
 from django.core.management.base import BaseCommand
 
@@ -13,8 +13,10 @@ from openedx.core.djangoapps.credentials.tasks.v1.tasks import backfill_date_for
 
 class Command(BaseCommand):
     """
-    A management command reponsible for populating the `certificate_available_date` field of
-    CourseCertificateConfiguration instances in the Credentials IDA.
+    A management command responsible for syncing certificate availability dates to Credentials using data from the CMS.
+
+    Example usage:
+        $ ./manage.py lms
     """
     def handle(self, *args, **options):
         backfill_date_for_all_course_runs.delay()
